d48fdc1d4484386faebf4c358887b726f77f3504
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-03-12 11:36:49 -0300

    Desabilitado menu na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { timestamp } from 'rxjs/operators';
 
 //vou poder referenciar essa classe com o nome dela entre "" (como String)
 @IonicPage()
@@ -11,11 +13,19 @@ import { IonicPage, NavController } from 'ionic-angular';
 export class HomePage {
 
   //objetos injetados na classe sao os passados no parametro do construtor
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
+    
+  }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
   }
 
   login() {
-    this.navCtrl.setRoot('CategoriasPage')
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
