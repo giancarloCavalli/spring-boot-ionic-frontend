048e63465cd6d88cb83a4d19cf5b2fee8be0ff59
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-03-12 07:08:46 -0300

    Apagando pagina List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,30 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//Decorator. Anotacao com configuracoes para alterar a classe
 @NgModule({
+  //lista de componentes ou paginas que fazem parte desse modulo
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
+  //define a dependencia de outros modulos
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
+  //como a aplicacao vai iniciar
   bootstrap: [IonicApp],
+  //quando for uma pagina, e nao somente um componente, tem que ter as mesmas informacoes que o atributo declaration
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
+  //define quais objetos que vc injetar nesse modulo vao ter uma instancia unica
   providers: [
     StatusBar,
     SplashScreen,
