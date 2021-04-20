a6dce6f58f64963d5c16e3a33b2e0999d5849144
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-04-19 21:18:01 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -22,6 +22,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  };
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -33,7 +37,7 @@ export class ProdutosPage {
     error => {
       loader.dismiss();
     })
-  };
+  }
 
   loadImageUrls() {
     for (var i=0; i<this.items.length; i++) {
@@ -57,4 +61,12 @@ export class ProdutosPage {
     loader.present();
     return loader;
   }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+
 }
